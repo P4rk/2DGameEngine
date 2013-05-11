f1aba8213bbe22e1f56ca3f35c0eee625d475d24
unknown <lukecorpe@gmail.com>
2013-05-11 11:26:19 +0100

    updated level to use Jbox world

--- crow-engine/src/main/java/com/lukecorpe/crow/engine/level/Level.java
+++ crow-engine/src/main/java/com/lukecorpe/crow/engine/level/Level.java
@@ -1,20 +1,14 @@
 package com.lukecorpe.crow.engine.level;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 import org.jbox2d.dynamics.World;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.geom.Vector2f;
 
 import com.lukecorpe.crow.engine.Component;
 import com.lukecorpe.crow.engine.Game;
 import com.lukecorpe.crow.engine.Options;
-import com.lukecorpe.crow.engine.interfaces.*;
-import com.lukecorpe.crow.engine.objects.GameObject;
 
 public class Level extends Component{
 	
@@ -26,38 +20,24 @@ public class Level extends Component{
 	
 	Background bg;
 	
-	HashMap<String, String> tileReference = new HashMap<String, String>();
+	World world;
 	
-	ArrayList<GameObject> objects = new  ArrayList<GameObject>();
+	boolean doSleep = true;
 	
-	//private Player player;
-	
-	private int tileHeight;
-	private int tileWidth;
-	
-    /*public Player getPlayer() {
-    	/*if(player==null){
-    		player = new Player(getGame());
-    		objects.add(player);
-    	}
-		return player;
-    	return null;
-	}*/
-	
-    public World getWorld(){
-    	//return world;
-    	return null;
-    }
-    //Jbox2d
-    World world;
+	float timeStep;
+    int velocityIterations;
+    int positionIterations;
     
 	public Level(Game game){
 		super(game);
-		/*bg = new Background(getGame());
+		bg = new Background(getGame());
 		
 		//Jbox2d
 		this.world = new World(Options.Instance.Gravity(), false);
 		
+		timeStep = 1.0f / 60.f;
+	    velocityIterations = 6;
+	    positionIterations = 2;
 		
 		/*getPlayer().setTexture("src/main/resources/Images/SpriteSheet.png", 75, 96);
 		getPlayer().addAnimation("Idle", new Vector2f(2, 2), new Vector2f(2, 2), 2, 1, true);
@@ -69,7 +49,7 @@ public class Level extends Component{
 	}
 
 	public void finalise() {
-		/*try{
+		try{
 	        try {
 				renderTarget = new Image(getWidth(), getHeight());
 				renderTargetGraph = renderTarget.getGraphics();
@@ -80,35 +60,36 @@ public class Level extends Component{
 			}
         }catch (SlickException e){
         	e.printStackTrace();
-        }
+        	throw new RuntimeException(e);
+        }/*
 		for(GameObject obj : objects){
 			obj.finalisePositions();
 		}*/
 	}
 	public void update(int delta) {
-		/*
+		
 		bg.update(delta);
-		world.step(1f, 8, 3);
-		for (GameObject obj : objects) {
+		world.step(timeStep, velocityIterations, positionIterations);
+		/*for (GameObject obj : objects) {
 			obj.update(delta);
 		}*/
 	}
 	public void draw(){
-		/*renderTargetGraph.setBackground(new Color(5, 34, 69));
+		renderTargetGraph.setBackground(new Color(5, 34, 69));
     	renderTargetGraph.clear();
     	
 		
 		bg.draw(renderTargetGraph);
-		for (GameObject obj : objects) {
+		/*for (GameObject obj : objects) {
 			obj.draw(renderTargetGraph);
-		}
+		}*/
 
-    	renderTargetGraph.flush();*/
+    	renderTargetGraph.flush();
 	}
-	public void addTileReference(String identifier, String path){
+	/*public void addTileReference(String identifier, String path){
 		//tileReference.put(identifier, path);
 	}
-	/*public String getTilePath(String key){
+	public String getTilePath(String key){
 		//return tileReference.get(key);
 	}*/
 	public int getWidth() {
@@ -125,18 +106,11 @@ public class Level extends Component{
 		this.height = height;
 	}
 
-	public ArrayList<GameObject> getObjects() {
-		//return objects;
-		return null;
-	}
+    public World getWorld(){
+    	return world;
+    }
 
 	public Image getRenderTarget() {
-		//return renderTarget;
-		return null;
+		return renderTarget;
 	}
-
-	public void setTileSizes(int maxTileHeight, int maxTileWidth) {
-		this.tileHeight = maxTileHeight;
-		this.tileWidth = maxTileWidth;
-	}	
 }
