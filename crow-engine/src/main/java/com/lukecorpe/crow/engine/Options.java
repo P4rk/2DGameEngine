a967ee5ec3dde6a20d8de24e011b553d8d394fa9
unknown <lukecorpe@gmail.com>
2013-05-12 16:31:55 +0100

    removed gravity from global options. Now per level basis.

--- crow-engine/src/main/java/com/lukecorpe/crow/engine/Options.java
+++ crow-engine/src/main/java/com/lukecorpe/crow/engine/Options.java
@@ -1,28 +1,12 @@
 package com.lukecorpe.crow.engine;
 
-import org.jbox2d.common.Vec2;
-
 public enum Options {
 	Instance; 
 
-	private Vec2 _gravity = new Vec2(0, 0.1f);
-	private float _wind = 0.0f;
 	private boolean _fullScreen=true;
 	private boolean _pasued=false;
 	private boolean _debug=true;
 	
-	public Vec2 Gravity(){
-		return _gravity;
-	}
-	public void setGravity(Vec2 g){
-		_gravity = g;
-	}
-	public void setWind(float _wind) {
-		this._wind = _wind;
-	}
-	public float Wind() {
-		return _wind;
-	}
 	public boolean isFullScreen() {
 		return _fullScreen;
 	}
