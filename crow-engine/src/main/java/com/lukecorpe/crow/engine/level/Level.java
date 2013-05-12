04b3722473f43af97e97911649587125ce556ed4
unknown <lukecorpe@gmail.com>
2013-05-12 16:28:33 +0100

    updated the level and levelload to be inline with new gameobject

--- crow-engine/src/main/java/com/lukecorpe/crow/engine/level/Level.java
+++ crow-engine/src/main/java/com/lukecorpe/crow/engine/level/Level.java
@@ -1,5 +1,14 @@
 package com.lukecorpe.crow.engine.level;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jbox2d.collision.shapes.PolygonShape;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.FixtureDef;
 import org.jbox2d.dynamics.World;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
@@ -9,6 +18,7 @@ import org.newdawn.slick.SlickException;
 import com.lukecorpe.crow.engine.Component;
 import com.lukecorpe.crow.engine.Game;
 import com.lukecorpe.crow.engine.Options;
+import com.lukecorpe.crow.engine.interfaces.Drawable;
 
 public class Level extends Component{
 	
@@ -24,21 +34,26 @@ public class Level extends Component{
 	
 	boolean doSleep = true;
 	
+	private List<Drawable> drawList;
+	
 	float timeStep;
     int velocityIterations;
     int positionIterations;
+    
+    Vec2 intiGravity = new Vec2(0, 98f);
     
 	public Level(Game game){
 		super(game);
 		bg = new Background(getGame());
 		
 		//Jbox2d
-		this.world = new World(Options.Instance.Gravity(), false);
+		this.world = new World(intiGravity, false);
 		
 		timeStep = 1.0f / 60.f;
 	    velocityIterations = 6;
 	    positionIterations = 2;
 		
+	    drawList = new ArrayList<Drawable>();
 		/*getPlayer().setTexture("src/main/resources/Images/SpriteSheet.png", 75, 96);
 		getPlayer().addAnimation("Idle", new Vector2f(2, 2), new Vector2f(2, 2), 2, 1, true);
 		getPlayer().addAnimation("Walk", new Vector2f(0, 0), new Vector2f(5, 1), 5, 15, true);
@@ -61,10 +76,24 @@ public class Level extends Component{
         }catch (SlickException e){
         	e.printStackTrace();
         	throw new RuntimeException(e);
-        }/*
+        }
+		//createBox();
+		/*
 		for(GameObject obj : objects){
 			obj.finalisePositions();
 		}*/
+	}
+
+	private void createBox() {
+		BodyDef bodyDef = new BodyDef();
+		bodyDef.type = BodyType.STATIC;
+		bodyDef.position.set(new Vec2());
+		Body body = this.getWorld().createBody(bodyDef);
+		PolygonShape shape = new PolygonShape();
+		shape.setAsBox(0, height);
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = shape;
+		body.createFixture(fixtureDef);
 	}
 	public void update(int delta) {
 		
@@ -78,11 +107,10 @@ public class Level extends Component{
 		renderTargetGraph.setBackground(new Color(5, 34, 69));
     	renderTargetGraph.clear();
     	
-		
 		bg.draw(renderTargetGraph);
-		/*for (GameObject obj : objects) {
+		for (Drawable obj : drawList) {
 			obj.draw(renderTargetGraph);
-		}*/
+		}
 
     	renderTargetGraph.flush();
 	}
@@ -112,5 +140,17 @@ public class Level extends Component{
 
 	public Image getRenderTarget() {
 		return renderTarget;
+	}
+
+	public List<Drawable> getDrawList() {
+		return drawList;
+	}
+
+	public Vec2 getGravity() {
+		return getWorld().getGravity();
+	}
+
+	public void setGravity(Vec2 gravity) {
+		getWorld().setGravity(gravity);
 	}
 }
--- crow-engine/src/main/java/com/lukecorpe/crow/engine/level/LevelLoader.java
+++ crow-engine/src/main/java/com/lukecorpe/crow/engine/level/LevelLoader.java
@@ -6,17 +6,22 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Map;
 
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.BodyType;
+import org.newdawn.slick.SlickException;
+
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lukecorpe.crow.engine.Game;
+import com.lukecorpe.crow.engine.objects.GameObject;
 
 public class LevelLoader {
 	public static void loadLevel(String filePath, Game game){
 		ObjectMapper mapper = new ObjectMapper();
 		try {
 			String fileSrc = parseFile(filePath);
-			Map levelMap = mapper.readValue(fileSrc, Map.class);
+			Map<String, Object> levelMap = mapper.readValue(fileSrc, Map.class);
 			createLevel(levelMap, game);
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -33,14 +38,37 @@ public class LevelLoader {
 		return totalString;
 	}
 	
-	private static void createLevel(Map levelMap, Game game) {
+	private static void createLevel(Map<String, Object> levelMap, Game game) {
 		Level level = new Level(game);
 		
 		level.setWidth((int) levelMap.get("width"));
 		level.setHeight((int) levelMap.get("height"));
 		
+		createObjects((Map)levelMap.get("levelObjects"), level);
+		
 		level.finalise();
 		game.getLevels().add(level);
+	}
+
+	private static void createObjects(Map<String, Object> objectsMap, Level level) {
+		for(String s : objectsMap.keySet()){
+			Map<String, Object> objectMap = (Map<String, Object>)objectsMap.get(s);
+			try {
+				GameObject obj = new GameObject(
+						level, 
+						BodyType.valueOf((String)objectMap.get("bodyType")), 
+						new Vec2(
+								Float.valueOf((String)objectMap.get("startPositionX")), 
+								Float.valueOf((String)objectMap.get("startPositionY"))
+							), 
+						(String)objectMap.get("relitaveImagePath")
+						);
+				obj.setDensity(Float.valueOf((String)objectMap.get("density")));
+				obj.setFriction(Float.valueOf((String)objectMap.get("friction")));
+			} catch (SlickException e) {
+				throw new RuntimeException(e);
+			}
+		}
 	}
 }
 
