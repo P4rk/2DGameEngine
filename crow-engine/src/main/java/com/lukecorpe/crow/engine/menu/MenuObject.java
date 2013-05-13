79e6f701bf817b0ccd6b145e496f2d4f2fe87551
Luke Corpe <luke.corpe@semantico.com>
2013-05-13 14:15:22 +0100

    updated MenuObject so it complies

--- crow-engine/src/main/java/com/lukecorpe/crow/engine/menu/MenuObject.java
+++ crow-engine/src/main/java/com/lukecorpe/crow/engine/menu/MenuObject.java
@@ -7,20 +7,13 @@ import com.lukecorpe.crow.engine.level.Level;
 import com.lukecorpe.crow.engine.math.MathUtils;
 import com.lukecorpe.crow.engine.objects.GameObject;
 
-public abstract class MenuObject extends GameObject {
-
-	public MenuObject(com.lukecorpe.crow.engine.Game game) {
-		//super(game);
-	}
-	public MenuObject(com.lukecorpe.crow.engine.Game game, BodyType type) {
-		//super(game, type);
-	}
+public abstract class MenuObject {
 	
-	@Override
+	
 	public void setPosition(Vector2f _position) {
 		//this._position = new Vector2f(MathUtils.clamp(_position.getX(), 0, getGame().getCurrentMenu().getWidth()-getBounding().getWidth()), MathUtils.clamp(_position.getY(), 0, getGame().getCurrentMenu().getHeight()-getBounding().getHeight()));
 	}
-	@Override
+	
 	public void setPosition(float x, float y) {
 		//this._position = new Vector2f(MathUtils.clamp(x, 0, getGame().getCurrentMenu().getWidth()-getBounding().getWidth()), MathUtils.clamp(y, 0, getGame().getCurrentMenu().getHeight()-getBounding().getHeight()));
 	}
